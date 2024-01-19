eb0886cbf00aeb531ab263d4419027e3360c1ad0
Matheus Souza <matheus3864@gmail.com>
2024-01-19 14:35:40 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,38 +1,54 @@
 import requests
 
-api_key = "b005303fb1b5163d9a7d30df0743db84"  # Substitua com a sua chave de API do TMDb
-url = "https://api.themoviedb.org/3/movie/popular"
+api_key = "b005303fb1b5163d9a7d30df0743db84"
 
 params = {
     "api_key": api_key,
-    "language": "pt-BR", 
+    "language": "pt-BR",
+    "page" : 1
 }
 
 def getGenres(params):
+    url_genre = "https://api.themoviedb.org/3/genre/movie/list"
     try:
-        response = requests.get(url, params=params)
+        response = requests.get(url_genre, params=params)
         response.raise_for_status() 
 
         return response.json()["genres"]
-
-        # Exibe os IDs e nomes dos gêneros
-    #     for genre in genres:
-    #         print(f"ID: {genre['id']} | Nome: {genre['name']}")
-
+    
     except requests.exceptions.RequestException as e:
-        print(f"Erro na solicitação: {e}")
+        return f"Erro na solicitação: {e}"
 
 
 def getFilmsAll(params):
-    response = requests.get(url, params=params)
-    response.raise_for_status()
+    url_film = "https://api.themoviedb.org/3/movie/popular"
+    try:
+        response = requests.get(url_film, params=params)
+        response.raise_for_status()
 
-    return response.json()["results"]
+        return response.json()["results"]
+    
+    except requests.exceptions.RequestException as e:
+        return f"Erro na solicitação: {e}"
 
+def listGenres(generos):
+    print("Gêneros")
+    for i in generos:
+        print(f"{i['id']} - {i['name']}")
+    return ''
 
-print(getGenres(params))
-# filmes =  getFilmsAll(params)
-# for filme in filmes:
-#     if filme["adult"]:
-#         print(filme['title'])
+generos = getGenres(params)
+filmes = getFilmsAll(params)
+id_genero = 0
 
+print(listGenres(generos))
+entrada = input('Selecione o id do genero: ')
+
+for i in generos:
+    if i['name'] == entrada:
+        id_genero = i['id']
+        break
+
+for k in filmes:
+    if id_genero in k['genre_ids']:
+        print(k['title'])
